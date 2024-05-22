3aca4bd35f2a2c3c326a0ec5509eba4c519e529f
laughingdog911 <estherghkd@gmail.com>
2024-05-22 21:39:51 +0900

    Fixed frg places, applied binding.

--- app/src/main/java/com/hj/mylogin/StartActivity.kt
+++ app/src/main/java/com/hj/mylogin/StartActivity.kt
@@ -1,21 +1,23 @@
 package com.hj.mylogin
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.RegisterFragment
+import androidx.databinding.DataBindingUtil
+import com.hj.mylogin.LoginFragment
+import com.hj.mylogin.databinding.ActivityStartBinding
+
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_start)
+        setContentView(R.layout.activity_start)
 
         val transactionManager = supportFragmentManager.beginTransaction()
-        transactionManager.replace(R.id.main, RegisterFragment())
+        transactionManager.replace(R.id.main, LoginFragment())
         transactionManager.commit()
-
-        }
     }
+
+
 }
